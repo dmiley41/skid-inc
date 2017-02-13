7dc304c41c941fab8d809f600ef266b5f5ebad3e
TotomInc <totom7700@gmail.com>
2017-02-13 20:53:34 +0000

    added themes and proper command

--- app/js/game/options.js
+++ app/js/game/options.js
@@ -1,7 +1,22 @@
 SkidInc.Options = {
-    options: ['background', 'typing'],
-    execPath: ['SkidInc.Options.toggleBackground', 'SkidInc.Options.toggleTyping'],
-    toggled: [true, false],
+    options: ['background', 'typing', 'theme'],
+    execPath: ['SkidInc.Options.toggleBackground', 'SkidInc.Options.toggleTyping', 'SkidInc.Options.changeTheme'],
+    toggled: [true, false, 'default'],
+    themes: ['default', 'fallout'],
+    themesLocked: [false, true],
+    
+    getThemes: function() {
+        var themes = '';
+        
+        for (var i = 0; i < SkidInc.Options.themes.length; i++) {
+            themes += SkidInc.Options.themes[i]
+            
+            if (i !== (SkidInc.Options.themes.length - 1))
+                themes += ', ';
+        };
+        
+        return themes;
+    },
     
     toggleBackground: function(param) {
         if (param == true) {
@@ -27,9 +42,23 @@ SkidInc.Options = {
         };
     },
     
+    changeTheme: function(param) {
+        if (SkidInc.Options.themesLocked[SkidInc.Options.themes.indexOf(param)])
+            return SkidInc.print('<b>' + param + '</b> theme is locked.');
+        else if (!SkidInc.Options.themesLocked[SkidInc.Options.themes.indexOf(param)]) {
+            $('.console').removeClass().addClass('console ' + param);
+            $('.navbar').removeClass().addClass('navbar navbar-default navbar-fixed-top ' + param);
+            
+            SkidInc.Options.toggled[SkidInc.Options.options.indexOf('theme')] = param;
+            
+            return SkidInc.print('<b>' + param + '</b> theme enabled.');
+        };
+    },
+    
     list: function() {
         var str = '<b>option background true/false</b>: enable/disable matrix background effect (disabling it increase performances).<br>' +
-            '<b>option typing true/false</b>: enable/disable the typing text effect.';
+            '<b>option typing true/false</b>: enable/disable the typing text effect.<br>' +
+            '<b>option theme [theme name]</b>: change your theme according to this list (<b>' + SkidInc.Options.getThemes() + '</b>).<br>';
         
         return SkidInc.print(str);
     },
@@ -37,6 +66,15 @@ SkidInc.Options = {
     exec: function(a) {
         if (SkidInc.Options.options.indexOf(a[0]) == -1)
             return SkidInc.print('<b>' + a[0] + '</b> is an unknown option, type <b>option -l</b> for a list of options.');
+        
+        // exception: theme
+        if (a[0] == 'theme') {
+            if (SkidInc.Options.themes.indexOf(a[1]) == -1)
+                return SkidInc.print('<b>' + a[1] + '</b> is an unknown theme, type <b>option -l</b> for a list of options, and themes.');
+            if (SkidInc.Options.themes.indexOf(a[1]) > -1)
+                return SkidInc.Options.changeTheme(a[1]);
+        };
+        
         if (a[1] !== 'true' && a[1] !== 'false')
             return SkidInc.print('You must put <b>true</b> or <b>false</b> as a parameter instead of <b>' + a[1] + '</b>.');
         if (a[1] == 'true' || a[1] == 'false') {
